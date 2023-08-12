fe5985a8525b58cdd89e065410316925bd9e0916
Mussi Agustin <agustinmfacultad@gmail.com>
2023-08-12 20:35:12 -0300

    Ahora si funciona

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const { inicializarDB } = require("./db_config");
 const app= express();
 
 
-//Activar el servidor
-port=8000;
-async()=>{
-    inicializarDB();
-    app.listen(port, ()=>{
-        console.log("Escuchando en el puerto 8080");
-    }) 
-}
+// Activar el servidor
+const port = 8000;
 
+// Función asíncrona para inicializar la base de datos y luego iniciar el servidor
+const iniciarServidor = async () => {
+    try {
+        await inicializarDB();
+        app.listen(port, () => {
+            console.log(`Escuchando en el puerto ${port}`);
+        });
+    } catch (error) {
+        console.error("Error al iniciar el servidor:", error);
+    }
+};
+
+iniciarServidor(); // Llama a la función para activar el servidor
